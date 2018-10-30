a891bac995cc7d26572097ec55ac80f3cab0e5b7
Nguyen Quang An <ngquangan@gmail.com>
2018-10-30 11:57:10 +0700

    Add handle error success

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+
 
 const productRoutes = require('./api/routes/products');
 const orderRouters = require('./api/routes/orders');
@@ -14,7 +17,28 @@ app.get('/', (req, res, next) => {
 
 });
 
+app.use(morgan('dev'));
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRouters);
 
+const createLog = (req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404; //status not method
+    next(error);
+}
+
+//Handing error
+app.use(createLog); //forword error
+
+const handleLog = (error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+};
+
+app.use(handleLog);
 module.exports = app;
